a7bc512a45661175b2eebc3237e14b22e6e1371b
zhangh4 <zhangh4@ihpc.a-star.edu.sg>
2024-09-20 13:12:44 +0800

    add star eval

---
+++ eval_star_zs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+MODEL_PATH=Efficient-Large-Model/VILA1.5-3B
+CKPT=result_kg
+
+python inference_test/evaluate_star.py \
+    --model-path $MODEL_PATH \
+    --max_new_tokens 512 \
+    --num_video_frames 1 \
+    --question-file ./dataset/star/sft_annots/STAR_val_NEAT_imgx4_3.0.json \
+    --image-folder ./dataset/star/charadesv1_480/frames \
+    --answers-file ./tmp/$MODEL_PATH \
+    --temperature 0 \
+    --conv-mode vicuna_v1
+
