6a5e46ae11377dbab1315caaa387a86e4dba75e1
zhangh4 <zhangh4@ihpc.a-star.edu.sg>
2024-09-22 14:14:57 +0800

    add evaluate 8

--- eval_star_zs.sh
+++ eval_star_zs.sh
@@ -5,8 +5,8 @@ CKPT=result_kg
 python inference_test/evaluate_star.py \
     --model-path $MODEL_PATH \
     --max_new_tokens 512 \
-    --num_video_frames 1 \
-    --question-file ./dataset/star/sft_annots/STAR_val_NEAT_imgx4_3.0.json \
+    --num_video_frames 8 \
+    --question-file ./dataset/star/sft_annots/STAR_val_NEAT_imgx8_3.0.json \
     --image-folder ./dataset/star/charadesv1_480/frames \
     --answers-file ./tmp/$MODEL_PATH \
     --temperature 0 \
