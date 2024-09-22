f09a5290086ad2b90c1e4736b464894b973e9231
zhangh4 <zhangh4@ihpc.a-star.edu.sg>
2024-09-22 14:15:27 +0800

    evaluate tuned

---
+++ eval_star_tune.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+MODEL_PATH=Efficient-Large-Model/VILA1.5-3B
+MODEL_PATH=ckpts/star_imgx4_vila3b
+MODEL_PATH=ckpts/star_imgx4_v3_imgx4_VILA1.5_3B_20480_linear_Bank0_LearnLat0
+CKPT=result_kg
+
+python inference_test/evaluate_star.py \
+    --model-path $MODEL_PATH \
+    --max_new_tokens 512 \
+    --num_video_frames 8 \
+    --question-file ./dataset/star/sft_annots/STAR_val_NEAT_imgx4_3.0.json \
+    --image-folder ./dataset/star/charadesv1_480/frames \
+    --answers-file ./tmp/$MODEL_PATH \
+    --temperature 0 \
+    --conv-mode vicuna_v1
+
