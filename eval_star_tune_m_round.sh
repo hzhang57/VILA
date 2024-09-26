1f1293b314f46dbd7999398bab8fd97f4389ead2
zhangh4 <zhangh4@ihpc.a-star.edu.sg>
2024-09-26 17:22:24 +0800

    add eval m round

--- eval_star_tune_m_round.sh
+++ eval_star_tune_m_round.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-MODEL_PATH=ckpts/star_imgx4_Q_Gen_Prog_vila3b_loss2
+MODEL_PATH=ckpts/star_qa_imgx4_Query_Image_Gen_Program_vila3b_loss2
 CKPT=result_kg
 
 python inference_test/evaluate_star_m_round.py \
     --model-path $MODEL_PATH \
     --max_new_tokens 512 \
     --num_video_frames 4 \
-    --question-file ./dataset/star/sft_annots/STAR_val_NEAT_imgx4_Q_Gen_Prog_v3.0.json \
+    --question-file ./dataset/star/sft_annots/STAR_val_NEAT_imgx4_Query_Image_Gen_Program_v3.0.json \
     --image-folder ./dataset/star/charadesv1_480/frames \
     --answers-file ./tmp/$MODEL_PATH \
     --temperature 0 \
