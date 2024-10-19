c272501105fc2a92c332bd7f5d4074ef5cccb2d8
hzhang57 <zhanghaoinf@outlook.com>
2024-10-19 23:44:23 +0800

    3.1 data

--- llava/data/datasets_mixture.py
+++ llava/data/datasets_mixture.py
@@ -161,7 +161,7 @@ def register_datasets_mixtures():
     star_qa_Query_Video = Dataset(
         dataset_name="star_qa_Query_Video",
         dataset_type="star_qa_decord",
-        data_path="./dataset/star/sft_annots_video_v3/STAR_train_NEAT_Query_Video_v3.0.json",
+        data_path="./dataset/star/sft_annots_video_v3.1/STAR_train_NEAT_Query_Video_v3.1.json",
         image_path="./dataset/star/charadesv1_480/video/",
         description="Original data source: https://bobbywu.com/STAR/",
     )
@@ -172,7 +172,7 @@ def register_datasets_mixtures():
     star_qa_Program_Video = Dataset(
         dataset_name="star_qa_Program_Video",
         dataset_type="star_qa_decord",
-        data_path="./dataset/star/sft_annots_video_v3/STAR_train_NEAT_Program_Video_v3.0.json",
+        data_path="./dataset/star/sft_annots_video_v3.1/STAR_train_NEAT_Program_Video_v3.1.json",
         image_path="./dataset/star/charadesv1_480/video/",
         description="Original data source: https://bobbywu.com/STAR/",
     )
@@ -183,7 +183,7 @@ def register_datasets_mixtures():
     star_qa_Query_Program_Video = Dataset(
         dataset_name="star_qa_Query_Program_Video",
         dataset_type="star_qa_decord",
-        data_path="./dataset/star/sft_annots_video_v3/STAR_train_NEAT_Query_Program_Video_v3.0.json",
+        data_path="./dataset/star/sft_annots_video_v3.1/STAR_train_NEAT_Query_Program_Video_v3.1.json",
         image_path="./dataset/star/charadesv1_480/video/",
         description="Original data source: https://bobbywu.com/STAR/",
     )
@@ -194,25 +194,25 @@ def register_datasets_mixtures():
     star_qa_Query_Gen_Program_Video = Dataset(
         dataset_name="star_qa_Query_Gen_Program_Video",
         dataset_type="star_qa_decord",
-        data_path="./dataset/star/sft_annots_video_v3/STAR_train_NEAT_Query_Gen_Program_Video_v3.0.json",
+        data_path="./dataset/star/sft_annots_video_v3.1/STAR_train_NEAT_Query_Gen_Program_Video_v3.1.json",
         image_path="./dataset/star/charadesv1_480/video/",
         description="Original data source: https://bobbywu.com/STAR/",
     )
     add_dataset(star_qa_Query_Gen_Program_Video)
 
-    star_qa_Query_Gen_Program_Middle_Video = Dataset(
-        dataset_name="star_qa_Query_Gen_Program_Middle_Video",
-        dataset_type="star_qa_decord",
-        data_path="./dataset/star/sft_annots_video_v3/STAR_train_NEAT_Query_Gen_Program_Middle_Video_v3.0.json",
-        image_path="./dataset/star/charadesv1_480/video/",
-        description="Original data source: https://bobbywu.com/STAR/",
-    )
-    add_dataset(star_qa_Query_Gen_Program_Middle_Video)
+    ###star_qa_Query_Gen_Program_Middle_Video = Dataset(
+    ###    dataset_name="star_qa_Query_Gen_Program_Middle_Video",
+    ###    dataset_type="star_qa_decord",
+    ###    data_path="./dataset/star/sft_annots_video_v3.1/STAR_train_NEAT_Query_Gen_Program_Middle_Video_v3.1.json",
+    ###    image_path="./dataset/star/charadesv1_480/video/",
+    ###    description="Original data source: https://bobbywu.com/STAR/",
+    ###)
+    ###add_dataset(star_qa_Query_Gen_Program_Middle_Video)
 
     star_qa_Query_Gen_Program_Middle_Video_3Stage = Dataset(
         dataset_name="star_qa_Query_Gen_Program_Middle_Video_3Stage",
         dataset_type="star_qa_decord",
-        data_path="./dataset/star/sft_annots_video_v3/STAR_train_NEAT_Query_Gen_Program_Middle_Video_3Stage_v3.0.json",
+        data_path="./dataset/star/sft_annots_video_v3.1/STAR_train_NEAT_Query_Gen_Program_Middle_Video_3Stage_v3.1.json",
         image_path="./dataset/star/charadesv1_480/video/",
         description="Original data source: https://bobbywu.com/STAR/",
     )
