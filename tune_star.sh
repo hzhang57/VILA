3989821bb097c65e26edcad736d772457eb3ce2d
hzhang57 <zhanghaoinf@outlook.com>
2024-10-19 23:44:53 +0800

    3.1

--- tune_star.sh
+++ tune_star.sh
@@ -18,54 +18,8 @@ NUM_FRAMES=4
 MODEL_BASE=Efficient-Large-Model/VILA1.5-3B
 # Final output checkpoint path
 
-
-#DATA_SELECT="star_qa_Query_Video+star_qa_Program_Video+star_qa_Query_Program_Video"
-#OUTPUT="./ckpts_vid/star_qa_imgx{$NUM_FRAMES}_Query_Video+Program_Video+Query_Program_Video_vila3b_loss2_epoch1"
-#
-#torchrun --nnodes=$n_node --nproc_per_node=$nproc --master_port=25001 \
-#    --master_addr $MASTER_ADDR --node_rank=$CURRENT_RANK \
-#    llava/train/train_mem.py \
-#    --deepspeed ./scripts/zero3.json \
-#    --model_name_or_path $MODEL_BASE \
-#    --model_max_length 4096 \
-#    --version v1 \
-#    --num_video_frames $NUM_FRAMES \
-#    --data_mixture $DATA_SELECT \
-#    --vision_tower google/siglip-so400m-patch14-384 \
-#    --mm_vision_select_feature cls_patch \
-#    --mm_projector mlp_downsample \
-#    --tune_vision_tower True \
-#    --tune_mm_projector True \
-#    --tune_language_model True \
-#    --mm_vision_select_layer -2 \
-#    --mm_use_im_start_end False \
-#    --mm_use_im_patch_token False \
-#    --image_aspect_ratio resize \
-#    --bf16 True \
-#    --output_dir $OUTPUT \
-#    --num_train_epochs 1 \
-#    --per_device_train_batch_size $bs \
-#    --per_device_eval_batch_size $bs \
-#    --gradient_accumulation_steps $accu_steps \
-#    --evaluation_strategy "no" \
-#    --save_strategy "steps" \
-#    --save_steps 1000 \
-#    --save_total_limit 1 \
-#    --learning_rate 1e-5 \
-#    --weight_decay 0. \
-#    --warmup_ratio 0.03 \
-#    --lr_scheduler_type "cosine" \
-#    --logging_steps 1 \
-#    --tf32 True \
-#    --gradient_checkpointing True \
-#    --dataloader_num_workers 16 \
-#    --lazy_preprocess True \
-#    --vflan_no_system_prompt True \
-#    --report_to wandb
-
-
 DATA_SELECT="star_qa_Query_Video"
-OUTPUT="./ckpts_vid/star_qa_imgx{$NUM_FRAMES}_Query_Video_vila3b_loss2_epoch3"
+OUTPUT="./ckpts_vid/star_qa_imgx{$NUM_FRAMES}_Query_Video_vila3b_loss2_e1_3.1"
 
 torchrun --nnodes=$n_node --nproc_per_node=$nproc --master_port=25001 \
     --master_addr $MASTER_ADDR --node_rank=$CURRENT_RANK \
@@ -88,7 +42,7 @@ torchrun --nnodes=$n_node --nproc_per_node=$nproc --master_port=25001 \
     --image_aspect_ratio resize \
     --bf16 True \
     --output_dir $OUTPUT \
-    --num_train_epochs 3 \
+    --num_train_epochs 1 \
     --per_device_train_batch_size $bs \
     --per_device_eval_batch_size $bs \
     --gradient_accumulation_steps $accu_steps \
@@ -109,96 +63,8 @@ torchrun --nnodes=$n_node --nproc_per_node=$nproc --master_port=25001 \
     --report_to wandb
     
 
-#DATA_SELECT="star_qa_Program_Video"
-#OUTPUT="./ckpts_vid/star_qa_imgx{$NUM_FRAMES}_Program_Video_vila3b_loss2_epoch1"
-#
-#torchrun --nnodes=$n_node --nproc_per_node=$nproc --master_port=25001 \
-#    --master_addr $MASTER_ADDR --node_rank=$CURRENT_RANK \
-#    llava/train/train_mem.py \
-#    --deepspeed ./scripts/zero3.json \
-#    --model_name_or_path $MODEL_BASE \
-#    --model_max_length 4096 \
-#    --version v1 \
-#    --num_video_frames $NUM_FRAMES \
-#    --data_mixture $DATA_SELECT \
-#    --vision_tower google/siglip-so400m-patch14-384 \
-#    --mm_vision_select_feature cls_patch \
-#    --mm_projector mlp_downsample \
-#    --tune_vision_tower True \
-#    --tune_mm_projector True \
-#    --tune_language_model True \
-#    --mm_vision_select_layer -2 \
-#    --mm_use_im_start_end False \
-#    --mm_use_im_patch_token False \
-#    --image_aspect_ratio resize \
-#    --bf16 True \
-#    --output_dir $OUTPUT \
-#    --num_train_epochs 1 \
-#    --per_device_train_batch_size $bs \
-#    --per_device_eval_batch_size $bs \
-#    --gradient_accumulation_steps $accu_steps \
-#    --evaluation_strategy "no" \
-#    --save_strategy "steps" \
-#    --save_steps 1000 \
-#    --save_total_limit 1 \
-#    --learning_rate 1e-5 \
-#    --weight_decay 0. \
-#    --warmup_ratio 0.03 \
-#    --lr_scheduler_type "cosine" \
-#    --logging_steps 1 \
-#    --tf32 True \
-#    --gradient_checkpointing True \
-#    --dataloader_num_workers 16 \
-#    --lazy_preprocess True \
-#    --vflan_no_system_prompt True \
-#    --report_to wandb
-#
-#DATA_SELECT="star_qa_Query_Program_Video"
-#OUTPUT="./ckpts_vid/star_qa_imgx{$NUM_FRAMES}_Query_Program_Video_vila3b_loss2_epoch1"
-#
-#torchrun --nnodes=$n_node --nproc_per_node=$nproc --master_port=25001 \
-#    --master_addr $MASTER_ADDR --node_rank=$CURRENT_RANK \
-#    llava/train/train_mem.py \
-#    --deepspeed ./scripts/zero3.json \
-#    --model_name_or_path $MODEL_BASE \
-#    --model_max_length 4096 \
-#    --version v1 \
-#    --num_video_frames $NUM_FRAMES \
-#    --data_mixture $DATA_SELECT \
-#    --vision_tower google/siglip-so400m-patch14-384 \
-#    --mm_vision_select_feature cls_patch \
-#    --mm_projector mlp_downsample \
-#    --tune_vision_tower True \
-#    --tune_mm_projector True \
-#    --tune_language_model True \
-#    --mm_vision_select_layer -2 \
-#    --mm_use_im_start_end False \
-#    --mm_use_im_patch_token False \
-#    --image_aspect_ratio resize \
-#    --bf16 True \
-#    --output_dir $OUTPUT \
-#    --num_train_epochs 1 \
-#    --per_device_train_batch_size $bs \
-#    --per_device_eval_batch_size $bs \
-#    --gradient_accumulation_steps $accu_steps \
-#    --evaluation_strategy "no" \
-#    --save_strategy "steps" \
-#    --save_steps 1000 \
-#    --save_total_limit 1 \
-#    --learning_rate 1e-5 \
-#    --weight_decay 0. \
-#    --warmup_ratio 0.03 \
-#    --lr_scheduler_type "cosine" \
-#    --logging_steps 1 \
-#    --tf32 True \
-#    --gradient_checkpointing True \
-#    --dataloader_num_workers 16 \
-#    --lazy_preprocess True \
-#    --vflan_no_system_prompt True \
-#    --report_to wandb
-
-DATA_SELECT="star_qa_Query_Video+star_qa_Program_Video+star_qa_Query_Gen_Program_Video"
-OUTPUT="./ckpts_vid/star_qa_imgx{$NUM_FRAMES}_Query_Video+Program_Video+Query_Gen_Program_Video_vila3b_loss2_epoch1"
+DATA_SELECT="star_qa_Program_Video"
+OUTPUT="./ckpts_vid/star_qa_imgx{$NUM_FRAMES}_Program_Video_vila3b_loss2_e1_3.1"
 
 torchrun --nnodes=$n_node --nproc_per_node=$nproc --master_port=25001 \
     --master_addr $MASTER_ADDR --node_rank=$CURRENT_RANK \
@@ -241,8 +107,142 @@ torchrun --nnodes=$n_node --nproc_per_node=$nproc --master_port=25001 \
     --vflan_no_system_prompt True \
     --report_to wandb
 
-DATA_SELECT="star_qa_Query_Video+star_qa_Program_Video+star_qa_Query_Gen_Program_Middle_Video"
-OUTPUT="./ckpts_vid/star_qa_imgx{$NUM_FRAMES}_Query_Video+Program_Video+Query_Gen_Program_Middle_Video_vila3b_loss2_epoch1"
+
+DATA_SELECT="star_qa_Query_Program_Video"
+OUTPUT="./ckpts_vid/star_qa_imgx{$NUM_FRAMES}_Query_Program_Video_vila3b_loss2_e1_3.1"
+
+torchrun --nnodes=$n_node --nproc_per_node=$nproc --master_port=25001 \
+    --master_addr $MASTER_ADDR --node_rank=$CURRENT_RANK \
+    llava/train/train_mem.py \
+    --deepspeed ./scripts/zero3.json \
+    --model_name_or_path $MODEL_BASE \
+    --model_max_length 4096 \
+    --version v1 \
+    --num_video_frames $NUM_FRAMES \
+    --data_mixture $DATA_SELECT \
+    --vision_tower google/siglip-so400m-patch14-384 \
+    --mm_vision_select_feature cls_patch \
+    --mm_projector mlp_downsample \
+    --tune_vision_tower True \
+    --tune_mm_projector True \
+    --tune_language_model True \
+    --mm_vision_select_layer -2 \
+    --mm_use_im_start_end False \
+    --mm_use_im_patch_token False \
+    --image_aspect_ratio resize \
+    --bf16 True \
+    --output_dir $OUTPUT \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size $bs \
+    --per_device_eval_batch_size $bs \
+    --gradient_accumulation_steps $accu_steps \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 1000 \
+    --save_total_limit 1 \
+    --learning_rate 1e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --tf32 True \
+    --gradient_checkpointing True \
+    --dataloader_num_workers 16 \
+    --lazy_preprocess True \
+    --vflan_no_system_prompt True \
+    --report_to wandb
+
+DATA_SELECT="star_qa_Query_Gen_Program_Video"
+OUTPUT="./ckpts_vid/star_qa_imgx{$NUM_FRAMES}_Query_Gen_Program_Video_vila3b_loss2_e1_3.1"
+
+torchrun --nnodes=$n_node --nproc_per_node=$nproc --master_port=25001 \
+    --master_addr $MASTER_ADDR --node_rank=$CURRENT_RANK \
+    llava/train/train_mem.py \
+    --deepspeed ./scripts/zero3.json \
+    --model_name_or_path $MODEL_BASE \
+    --model_max_length 4096 \
+    --version v1 \
+    --num_video_frames $NUM_FRAMES \
+    --data_mixture $DATA_SELECT \
+    --vision_tower google/siglip-so400m-patch14-384 \
+    --mm_vision_select_feature cls_patch \
+    --mm_projector mlp_downsample \
+    --tune_vision_tower True \
+    --tune_mm_projector True \
+    --tune_language_model True \
+    --mm_vision_select_layer -2 \
+    --mm_use_im_start_end False \
+    --mm_use_im_patch_token False \
+    --image_aspect_ratio resize \
+    --bf16 True \
+    --output_dir $OUTPUT \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size $bs \
+    --per_device_eval_batch_size $bs \
+    --gradient_accumulation_steps $accu_steps \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 1000 \
+    --save_total_limit 1 \
+    --learning_rate 1e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --tf32 True \
+    --gradient_checkpointing True \
+    --dataloader_num_workers 16 \
+    --lazy_preprocess True \
+    --vflan_no_system_prompt True \
+    --report_to wandb
+
+DATA_SELECT="star_qa_Query_Gen_Program_Middle_Video_3Stage"
+OUTPUT="./ckpts_vid/star_qa_imgx{$NUM_FRAMES}_Query_Gen_Program_Middle_Video_3Stage_vila3b_loss2_e1_3.1"
+
+torchrun --nnodes=$n_node --nproc_per_node=$nproc --master_port=25001 \
+    --master_addr $MASTER_ADDR --node_rank=$CURRENT_RANK \
+    llava/train/train_mem.py \
+    --deepspeed ./scripts/zero3.json \
+    --model_name_or_path $MODEL_BASE \
+    --model_max_length 4096 \
+    --version v1 \
+    --num_video_frames $NUM_FRAMES \
+    --data_mixture $DATA_SELECT \
+    --vision_tower google/siglip-so400m-patch14-384 \
+    --mm_vision_select_feature cls_patch \
+    --mm_projector mlp_downsample \
+    --tune_vision_tower True \
+    --tune_mm_projector True \
+    --tune_language_model True \
+    --mm_vision_select_layer -2 \
+    --mm_use_im_start_end False \
+    --mm_use_im_patch_token False \
+    --image_aspect_ratio resize \
+    --bf16 True \
+    --output_dir $OUTPUT \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size $bs \
+    --per_device_eval_batch_size $bs \
+    --gradient_accumulation_steps $accu_steps \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 1000 \
+    --save_total_limit 1 \
+    --learning_rate 1e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --tf32 True \
+    --gradient_checkpointing True \
+    --dataloader_num_workers 16 \
+    --lazy_preprocess True \
+    --vflan_no_system_prompt True \
+    --report_to wandb
+
+
+DATA_SELECT="star_qa_Query_Gen_Program_Middle_Video_3Stage+star_qa_Query_Video+star_qa_Program_Video+star_qa_Query_Program_Video+star_qa_Query_Gen_Program_Video"
+OUTPUT="./ckpts_vid/star_qa_imgx{$NUM_FRAMES}_QVGenP+QV+PV+QPV+3Stage_vila3b_loss2_e1_3.1"
 
 torchrun --nnodes=$n_node --nproc_per_node=$nproc --master_port=25001 \
     --master_addr $MASTER_ADDR --node_rank=$CURRENT_RANK \
