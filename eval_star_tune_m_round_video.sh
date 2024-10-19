2b5b7576279c82945a05ed9ae5d06aabe3aebd81
zhangh4 <zhangh4@ihpc.a-star.edu.sg>
2024-10-19 18:07:47 +0800

    fix evaluatuion

--- eval_star_tune_m_round_video.sh
+++ eval_star_tune_m_round_video.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
-MODEL_PATH=ckpts_vid/star_qa_imgx{4}_Query_Video_Gen_Program_vila3b_loss2
-MODEL_PATH=ckpts_vid/star_qa_imgx{4}_Query_Video_Gen_Program_Middle_vila3b_loss2
-MODEL_PATH=ckpts_vid/star_qa_imgx{4}_Query_Video_Gen_Program_Middle_OptEarly_vila3b_loss2
-MODEL_PATH=ckpts_vid/star_qa_imgx{4}_Query_Video_Gen_Program_Middle_NoTab_vila3b_loss2
-MODEL_PATH=ckpts_vid/star_qa_imgx{4}_Query_Video_Gen_Program_Middle_NoTabOptEarly_vila3b_loss2
-MODEL_PATH=ckpts_vid/star_qa_imgx{4}_Query_Video_Gen_Program_Middle_NoTabOptEarlyFix_vila3b_loss2
+MODEL_PATH=ckpts_vid/star_qa_imgx{4}_Query_Gen_Program_Video_vila3b_loss2_epoch1
 CKPT=result_kg
 
 python inference_test/evaluate_star_m_round_video.py \
     --model-path $MODEL_PATH \
     --max_new_tokens 512 \
     --num_video_frames 4 \
-    --question-file ./dataset/star/sft_annots_video/STAR_val_NEAT_Query_Video_Gen_Program_Middle_NoTabOptEarlyFix_v5.4.json \
+    --question-file ./dataset/star/sft_annots_video_v3/STAR_val_NEAT_Query_Gen_Program_Video_v3.0.json \
     --image-folder ./dataset/star/charadesv1_480/video \
     --answers-file ./tmp/$MODEL_PATH \
     --temperature 0 \
