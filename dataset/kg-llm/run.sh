d825f3db177902940b2a0cfc68e49315c6310478
zhangh4 <zhangh4@ihpc.a-star.edu.sg>
2024-10-19 19:17:47 +0800

    update kg

---
+++ dataset/kg-llm/run.sh
@@ -0,0 +1,3 @@
+python gen_sft_json.py --input rephrased_QA_15Oct24/training.json
+python gen_sft_json.py --input rephrased_QA_15Oct24/testing.json
+python gen_sft_json.py --input rephrased_QA_15Oct24/validation.json
