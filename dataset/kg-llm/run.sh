8ca405d587f538794cf012b5bd7dc913469b38d1
zhangh4 <zhangh4@ihpc.a-star.edu.sg>
2024-10-20 15:09:05 +0800

    fix kg, return to 3.1

--- dataset/kg-llm/run.sh
+++ dataset/kg-llm/run.sh
@@ -1,3 +1,3 @@
-python gen_sft_json.py --input rephrased_QA_15Oct24/testing.json
-python gen_sft_json.py --input rephrased_QA_15Oct24/validation.json
-python gen_sft_json.py --input rephrased_QA_15Oct24/training.json
+python gen_sft_json.py --input rephrased_QA_15Oct24_v2/testing.json
+python gen_sft_json.py --input rephrased_QA_15Oct24_v2/validation.json
+python gen_sft_json.py --input rephrased_QA_15Oct24_v2/training.json
