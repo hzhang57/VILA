f0d78f589eac2fbfa040881780ea5de9485e3082
zhangh4 <zhangh4@ihpc.a-star.edu.sg>
2024-11-04 13:59:21 +0800

    add run.sh

--- dataset/kg-llm/run.sh
+++ dataset/kg-llm/run.sh
@@ -1,3 +1,3 @@
-python gen_sft_json.py --input rephrased_QA_15Oct24_v2/testing.json
-python gen_sft_json.py --input rephrased_QA_15Oct24_v2/validation.json
-python gen_sft_json.py --input rephrased_QA_15Oct24_v2/training.json
+python gen_sft_json.py --input rephrased_QA_25Oct24_v2.2/testing.json
+python gen_sft_json.py --input rephrased_QA_25Oct24_v2.2/validation.json
+python gen_sft_json.py --input rephrased_QA_25Oct24_v2.2/training.json
